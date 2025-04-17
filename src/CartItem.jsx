6b353fea2b6c15da292e6331a622f1f10ee307ce
MuhammadFaizan5478 <fsservice5478@gmail.com>
2025-04-17 16:01:35 +0300

    fix some directory

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { removeItem, updateQuantity } from '../redux/CartSlice';
+import { removeItem, updateQuantity } from './CartSlice';
 
 const CartItem = ({ item, onContinueShopping }) => {
   const dispatch = useDispatch();
