25ec099735c447c20c121c26a382a5f1b7c15833
MuhammadFaizan5478 <fsservice5478@gmail.com>
2025-04-17 18:55:11 +0300

    fix error 404 on git hub

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter } from 'react-router-dom'; // ✅ Import HashRouter
 import { Provider } from 'react-redux';
-import store from './redux/store';
 import App from './App';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import store from './store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/e-plantShopping">
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
-)
+);
